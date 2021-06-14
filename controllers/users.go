f9494ac28672d1adfb4b24f068c38708d561880a
Shou Tseng <yakushou730@gmail.com>
2021-06-14 09:49:40 +0800

    7.3.2 Moving the sign up page code

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,24 @@
 package controllers
 
-type Users struct{}
+import (
+	"net/http"
+
+	"github.com/yakushou730/go-web-development/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
