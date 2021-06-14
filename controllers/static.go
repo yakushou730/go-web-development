b81a14538b253896428353f2f24a6cad4048a8c5
Shou Tseng <yakushou730@gmail.com>
2021-06-14 15:48:42 +0800

    7.5.2 Simplifying the creation of new views

--- controllers/static.go
+++ controllers/static.go
@@ -4,9 +4,9 @@ import "github.com/yakushou730/go-web-development/views"
 
 func NewStatic() *Static {
 	return &Static{
-		Home:    views.NewView("bootstrap", "views/static/home.gohtml"),
-		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
-		Faq:     views.NewView("bootstrap", "views/static/faq.gohtml"),
+		Home:    views.NewView("bootstrap", "static/home"),
+		Contact: views.NewView("bootstrap", "static/contact"),
+		Faq:     views.NewView("bootstrap", "static/faq"),
 	}
 }
 
--- controllers/users.go
+++ controllers/users.go
@@ -14,7 +14,7 @@ type SignupForm struct {
 
 func NewUsers() *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+		NewView: views.NewView("bootstrap", "users/new"),
 	}
 }
 
